829df0c132b2645deeb0adc290d1a7cc49dc9350
neerajkumarTTN <neeraj.kumar@tothenew.com>
2021-10-08 20:22:23 +0530

    boolean solution

--- Q6.py
+++ Q6.py
@@ -1,9 +1,13 @@
 sideA=float(input("Enter the lenght of first side of triangle: "))
 sideB=float(input("Enter the lenght of second side of triangle: "))
 sideC=float(input("Enter the lenght of third side of triangle: "))
-if sideA==sideB and sideB==sideC:
-    print("Triangle is Equilateral")
-elif sideA==sideB or sideB==sideC or sideA==sideC:
-    print("Triangle is Isosceles")
+if sideA+sideB <=sideC or sideA+sideC <=sideB or sideB+sideC <=sideA :
+    print("Triangle is not valid")
 else:
-    print("Triangle is Scalane")
+    if sideA==sideB and sideB==sideC :
+        print("Triangle is Equilateral")
+    elif sideA==sideB or sideB==sideC or sideA==sideC:
+        print("Triangle is Isosceles")
+    else:
+        print("Triangle is Scalane")
+
